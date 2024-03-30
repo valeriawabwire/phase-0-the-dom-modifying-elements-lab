fbbe7bb38bd2a8ffb0a65bffecb787d33305e89d
valeria wabwire <larriebliss@gmail.com>
2024-03-30 14:37:48 +0300

    dom ME

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main = document.querySelector('#main');
+console.log(main);
+main.remove(main)
+let newHeader = document.querySelector('h1#victory');
+console.log(newHeader);
+newHeader.textContent= "Valeriais the champion";
+console.log(newHeader.textContent);
